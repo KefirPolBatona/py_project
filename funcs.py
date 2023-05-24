c4994544b8ad0eba73ea65baa9956dbfc825e49b
evgeni <9232485@gmail.com>
2023-05-24 10:52:50 +0300

    commit_funcs.py_2

--- funcs.py
+++ funcs.py
@@ -3,3 +3,10 @@ def sum_numbers(number_1, number_2):
     Takes 2 numbers and counts their sum
     """
     return number_1 + number_2
+
+
+def sub_numbers(number_1, number_2):
+    """
+    Takes 2 numbers and subtracts one number from another
+    """
+    return number_1 - number_2
